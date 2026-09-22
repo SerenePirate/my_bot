bb4b81ea2d6c42edf73bc4e39e54ee5e263df68c
Haasini <saihaasini.r@gmail.com>
2026-09-23 00:36:03 +0530

    Created gazebo_control plugin

---
+++ launch/launch_sim.launch.py
@@ -0,0 +1,63 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+
+
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+
+from launch_ros.actions import Node
+
+
+
+def generate_launch_description():
+
+    # Your robot package
+    package_name = 'my_bot'
+
+    # Include the robot_state_publisher launch file
+    rsp = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([
+            os.path.join(
+                get_package_share_directory(package_name),
+                'launch',
+                'rsp.launch.py'
+            )
+        ]),
+        launch_arguments={'use_sim_time': 'true'}.items()
+    )
+
+    # Include Gazebo Sim
+    gazebo = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([
+            os.path.join(
+                get_package_share_directory('ros_gz_sim'),
+                'launch',
+                'gz_sim.launch.py'
+            )
+        ]),
+        launch_arguments={
+            'gz_args': '-r empty.sdf'
+        }.items()
+    )
+
+
+     # Spawn robot from /robot_description
+    spawn_entity = Node(
+        package='ros_gz_sim',
+        executable='create',
+        arguments=[
+            '-topic', 'robot_description',
+            '-name', 'my_bot',
+            '-z', '0.1'
+        ],
+        output='screen'
+    )
+
+    # Launch everything
+    return LaunchDescription([
+        rsp,
+        gazebo,
+        spawn_entity,
+    ])
